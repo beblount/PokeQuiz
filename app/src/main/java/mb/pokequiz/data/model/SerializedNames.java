ebac8427e0e1af0ff9c820738e07538bf61a5451
mbpeele <mbpeele@email.wm.edu>
2016-02-01 14:12:15 -0500

    Update launcher icon, android icon gets annoying

--- app/src/main/java/mb/pokequiz/data/model/SerializedNames.java
+++ app/src/main/java/mb/pokequiz/data/model/SerializedNames.java
@@ -3,7 +3,7 @@ package mb.pokequiz.data.model;
 /**
  * Created by mbpeele on 2/1/16.
  */
-public class SerializedNames {
+class SerializedNames {
 
     public final static String RESOURCE_URI = "resource_uri";
     public final static String POKEDEX_ITEMS = "pokemon";
